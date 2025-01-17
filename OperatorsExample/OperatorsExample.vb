33e33bf240a9c36eec1aec3cce39d004f245d4bb
Noah Holloway <noahholloway23@gmail.com>
2025-01-17 08:21:34 -0700

    add more operators example

--- OperatorsExample/OperatorsExample.vb
+++ OperatorsExample/OperatorsExample.vb
@@ -1,7 +1,33 @@
-﻿Module OperatorsExample
+﻿'Noah Holloway
+'Spring 2025
+'RCET 2265
+'OperatorsExample
+'https://github.com/hollnoah/OperatorsExample.git
+
+
+Module OperatorsExample
 
     Sub Main()
 
+        'sum operator
+        Console.Write("5 + 3 = ") 'print a string
+        Console.WriteLine(5 + 3) 'print the sum
+        Console.WriteLine()
+
+        'subtraction operator
+        Console.Write("5 - 3 = ") 'print a string
+        Console.WriteLine(5 - 3) 'print the difference
+        Console.WriteLine()
+
+        'multiplication operator
+        Console.Write("5 * 3 = ") 'print a string
+        Console.WriteLine(5 * 3) 'print the product
+        Console.WriteLine()
+
+        'division operator
+        Console.Write("5 / 3 = ") 'print a string
+        Console.WriteLine(5 / 3) 'print the quotient
+        Console.WriteLine()
     End Sub
 
 End Module
