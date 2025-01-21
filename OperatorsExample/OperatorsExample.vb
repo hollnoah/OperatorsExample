5e9a4e8348449d52fbc5087e40f441041a7ee3d1
Noah Holloway <noahholloway23@gmail.com>
2025-01-21 08:44:34 -0700

    Comparison and equivocation code

--- OperatorsExample/OperatorsExample.vb
+++ OperatorsExample/OperatorsExample.vb
@@ -4,6 +4,8 @@
 'OperatorsExample
 'https://github.com/hollnoah/OperatorsExample.git
 
+Option Compare Text
+
 
 Module OperatorsExample
 
@@ -48,6 +50,39 @@ Module OperatorsExample
         Console.WriteLine("5" & " " & "+" & " " & "3" & " " & "=" & " " & "8") 'print a string
         Console.WriteLine()
         Console.WriteLine("wake up neo..." & vbNewLine & "follow the white rabbit...")
+        Console.WriteLine()
+
+        'Compare
+        Console.Write("5 > 3 = ") 'print a string
+        Console.WriteLine(5 > 3) 'print the comparison
+        Console.WriteLine()
+
+        'Compare
+        Console.Write("5 < 3 = ") 'print a string
+        Console.WriteLine(5 < 3) 'print the comparison
+        Console.WriteLine()
+
+        'question
+        Console.Write("does 5 = 3 ? ") 'print a string
+        Console.WriteLine(5 = 3) 'are these equal
+        Console.WriteLine()
+
+        'question
+        Console.Write("is 5 not equal to 3? ") 'print a string
+        Console.WriteLine(5 <> 3) 'are these different
+        Console.WriteLine()
+
+        'Compare
+        Console.Write("A > B = ") 'print a string
+        Console.WriteLine("A" > "B") 'print the comparison
+        Console.WriteLine()
+
+        'Compare
+        Console.Write("is A the same as a? ") 'print a string
+        Console.WriteLine("A" = "a") 'print the comparison
+        Console.WriteLine()
+
+
     End Sub
 
 End Module
